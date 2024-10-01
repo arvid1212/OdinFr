5bf8a42ab0e5154c1d70a46e26d048ec317272fa
arvid1212 <145985929+arvid1212@users.noreply.github.com>
2024-10-01 13:20:01 +0200

    first commit

---
+++ .js/script.js
@@ -0,0 +1,124 @@
+
+let namn="Arvid";
+let surname="Nilsson";
+
+let age=27;
+
+
+console.log("hello" + namn + surname + age + "nice to meet you" );
+
+age =28;
+
+console.log(age);
+
+let pi=3.14;
+
+pi= 10;
+
+console.log(pi);
+
+// assignment numbers
+
+console.log(23+97);
+// = 120
+
+console.log(1+3 - 5 + 7 - 2 +1);
+// = 5
+
+console.log((4+6+9) /77); 
+// = 0.24675324675324675
+
+let a = 10 
+
+console.log(a)
+// = 10
+
+console.log(9*a);
+// =90
+let b = 7*a;
+
+console.log(b)
+// =70
+
+const max = 57;
+
+let actual = max-13;
+
+let percentage = actual/max;
+
+console.log(percentage);
+// = 0.7719298245614035
+
+/*
+Q:
+Name the three ways to declare a variable
+
+A: Var,Let,Const
+
+Q:
+Which of the three variable declarations should you avoid and why?
+A:
+
+Var is outdated, Let is more modern, Const is only unchangeable variables
+Q:
+What rules should you follow when naming variables?
+A: lowercase, easy to understand, no reserevd names
+Q:
+What happens when you add numbers and strings together? 
+A: numbers become strings
+Q:
+How does the Modulo (%), or Remainder, operator work?   '
+
+A: Show whats left in the bigger number division  
+Q:
+Explain the difference between == and ===.
+A:
+// == same name, === same type
+Q
+When would you receive a NaN result?
+A: When you have not declared a varible number
+Q:
+How do you increment and decrement a number?
+A: 
+Let counter = 2
+counter++;
+counter--;
+Q:
+Explain the difference between prefixing and postfixing increment/decrement operators.
+
+A: 
+
+If the result of increment/decrement is not used, there is no difference in which form to use:
+
+If we’d like to increase a value and immediately use the result of the operator, we need the prefix form:
+
+If we’d like to increment a value but use its previous value, we need the postfix form:
+
+Q:
+What is operator precedence and how is it handled in JS?
+
+A: From school, we all know that the multiplication in the expression 1 + 2 * 2 should be calculated before the addition. That’s exactly the precedence thing. The multiplication is said to have a higher precedence than the addition.
+
+Parentheses override any precedence, so if we’re not satisfied with the default order, we can use them to change it. For example, write (1 + 2) * 2.
+
+Q;
+
+How do you access developer tools and the console?
+
+vs code:
+Save
+Go live index.html
+Browser:
+
+inspect
+
+console
+
+Q:
+
+How do you log information to the console?
+What does unary plus operator do to string representations of integers? eg. +”10”
+
+A: The unary plus or, in other words, the plus operator + applied to a single value, doesn’t do anything to numbers. But if the operand is not a number, the unary plus converts it into a number.
+
+*/ 
